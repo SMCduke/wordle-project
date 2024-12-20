02f1583cb893f82b59c454e806fdb61d2e9bd7fa
slade <slade.cornell@icloud.com>
2024-12-20 15:46:30 -0700

    more code added

--- wordle.py
+++ wordle.py
@@ -2,19 +2,21 @@ import random
 
 print("You have six guesses to figure out a five letter word")
 
-word_list = ["apple", "berry", "charm", "daisy", "eagle", "happy", "mayor", "green"]
+word_list = ["apple", "berry", "charm", "daisy", "eagle", "happy", "mayor", "green", "alpha", "every", "nicer"]
 
 chosen_word = random.choice(word_list)
 
 
-for x in range(6):
+for x in range(1,7):
     guess = input("Guess here: ")
+    guess = guess.lower()
     print(x)
 
     if guess == chosen_word:
         print(chosen_word)
-        print("Congrat you win!")
+        print("Congrats you win!")
         break
+    
     
     
     
