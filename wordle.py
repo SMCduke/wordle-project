25299eae666bb4b2d1ff52366a62724c47d1e5a9
slade <slade.cornell@icloud.com>
2024-12-19 17:57:58 -0700

    code fixed

--- wordle.py
+++ wordle.py
@@ -1,18 +1,35 @@
 import random
 
+print("You have six guesses to figure out a five letter word")
+word_guessed = input("Guess here: ")
+
 word_list = ["apple", "berry", "charm", "daisy", "eagle"]
 
 chosen_word = random.choice(word_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Color to letters
 
 
 guess = "words"
-past_guesses = []
+guesses = []
 guesses.append(guess)
 
 # guesses now equals ["words"]
 
 # print each letter and index and index in a word
-for i in range(len(word)):
-    print(i, word[i])
+for i in range(len(guess)):
+    print(i, guess[i])
